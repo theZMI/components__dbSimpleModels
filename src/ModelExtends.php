785c603870aa93999b7f7419650fe33662855fa9
theZMI <zmigpost@gmail.com>
2020-04-02 21:45:44 +0200

    Append into magic method for show data

--- src/ModelExtends.php
+++ src/ModelExtends.php
@@ -3,6 +3,9 @@
 namespace Models;
 
 class ModelExtends extends ModelOptimized {
+    const PAGE_ALL = -1;
+    const PAGE_LIMIT = 100;
+
     protected function CreateTable() {
     }
 
@@ -28,5 +31,20 @@ class ModelExtends extends ModelOptimized {
 
     public function __debugInfo() {
         return $this->GetData();
+    }
+
+    public function GetList($page = self::PAGE_ALL) {
+        $from = $page ? (intval($page - 1) * static::PAGE_LIMIT) : 0;
+        $ids = $this->db->selectCol(
+            "SELECT `id` FROM ?# WHERE 1 ORDER BY `create_time` DESC {LIMIT ?d}{, ?d}",
+            $this->table,
+            $page === self::PAGE_ALL ? DBSIMPLE_SKIP : $from,
+            $page === self::PAGE_ALL ? DBSIMPLE_SKIP : static::PAGE_LIMIT
+        );
+        $ret = [];
+        foreach ($ids as $id) {
+            $ret[$id] = new static($id);
+        }
+        return $ret;
     }
 }
